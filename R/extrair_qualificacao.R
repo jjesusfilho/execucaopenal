a49c0069b560073151b7e71d8a105a01ed67fcf4
José de Jesus Filho <jjesusfilho@gmail.com>
2020-06-15 12:38:37 -0300

    ajuste em extrair_qualificacao

--- R/extrair_qualificacao.R
+++ R/extrair_qualificacao.R
@@ -5,60 +5,67 @@
 #' @return tibble
 #' @export
 #'
-extrair_qualificacao <- function(indice){
-
-  indice <-  indice %>%
-    dplyr::filter(stringr::str_detect(doc,"atestado de pena"))
-
+extrair_qualificacao <- function (indice)
+{
+  indice <- indice %>% dplyr::filter(stringr::str_detect(doc,
+                                                         "atestado de pena"))
   processo <- unique(indice$processo)
 
   id_execucao <- unique(indice$numero)
 
   atestado <- indice$texto[1]
 
-  gerado_em <- stringr::str_extract(indice$texto[1],"(?<=Gerado em:).+?(?=por)") %>%
+  gerado_em <-
+    stringr::str_extract(indice$texto[1], "(?<=Gerado em:).+?(?=por)") %>%
     stringr::str_trim()
 
-  local_prisao <- stringr::str_extract(atestado,"(?<=\\(SEAP\\)\\*\\:).+") %>%
+  local_prisao <-
+    stringr::str_extract(atestado, "Local de Prisão.+") %>%
+    stringr::str_remove(".+?:") %>%
     stringr::str_trim()
 
-  codigo <- stringr::str_extract(atestado,"(?<=Código:).+") %>%
+  codigo <- stringr::str_extract(atestado, "(?<=Código:).+") %>%
     stringr::str_trim()
 
-  nome <- stringr::str_extract(atestado,"(?<=Nome:).+") %>%
+  nome <- stringr::str_extract(atestado, "(?<=Nome:).+") %>%
     stringr::str_trim()
 
-  mae <- stringr::str_extract(atestado,"(?<=Mãe:).+") %>%
+  mae <- stringr::str_extract(atestado, "(?<=Mãe:).+") %>%
     stringr::str_trim()
 
-  pai <- stringr::str_extract(atestado,"(?<=Pai:).+") %>%
+  pai <- stringr::str_extract(atestado, "(?<=Pai:).+") %>%
     stringr::str_trim()
 
-  data_nascimento <- stringr::str_extract(atestado,"(?<=Nascimento:).+(?=Naturalidade)") %>%
-    stringr::str_trim() %>%
-    lubridate::dmy()
+  data_nascimento <-
+    stringr::str_extract(atestado, "(?<=Nascimento:).+(?=Naturalidade)") %>%
+    stringr::str_trim() %>% lubridate::dmy()
 
-  sexo <- stringr::str_extract(atestado,"(?<=Sexo:).+") %>%
+  sexo <- stringr::str_extract(atestado, "(?<=Sexo:).+") %>%
     stringr::str_trim()
 
-  naturalidade <- stringr::str_extract(atestado,"(?<=Naturalidade:).+") %>%
+  naturalidade <-
+    stringr::str_extract(atestado, "(?<=Naturalidade:).+") %>%
     stringr::str_trim()
 
-  nacionalidade <- stringr::str_extract(atestado,"(?<=Nacionalidade:).+?(?=Estado)") %>%
+  nacionalidade <-
+    stringr::str_extract(atestado, "(?<=Nacionalidade:).+?(?=Estado)") %>%
     stringr::str_trim()
 
-  escolaridade <- stringr::str_extract(atestado,"(?<=Escolaridade:).+(?=Profissão)") %>%
+  escolaridade <-
+    stringr::str_extract(atestado, "(?<=Escolaridade:).+(?=Profissão)") %>%
     stringr::str_trim()
 
-  profissao <- stringr::str_extract(atestado,"(?<=Profissão:).+") %>%
+  profissao <-
+    stringr::str_extract(atestado, "(?<=Profissão:).+") %>%
     stringr::str_trim()
 
-  estado_civil <- stringr::str_extract(atestado,"(?<=Civil:).+") %>%
+  estado_civil <-
+    stringr::str_extract(atestado, "(?<=Civil:).+") %>%
     stringr::str_trim()
 
-  pena_imposta <- stringr::str_extract(atestado,"(?<=Imposta:).+") %>%
+  pena_imposta <-
+    stringr::str_extract(atestado, "(?<=Imposta:).+") %>%
     stringr::str_trim()
-
 
   tibble::tibble(
     processo = processo,
@@ -76,8 +83,5 @@ extrair_qualificacao <- function(indice){
     profissao = profissao,
     estado_civil = estado_civil,
     pena_imposta = pena_imposta,
-
   )
-
-
 }
